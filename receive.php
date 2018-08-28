31c880fd15f5d2a57b788aaf2d500dd52ecfc9ef
xtony77 <xtony77@gmail.com>
2018-08-28 09:52:10 +0800

    modify retry receive queue

--- receive.php
+++ receive.php
@@ -2,35 +2,50 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-$channel = $connection->channel();
+function receiveQueue($queuesName) 
+{
+    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $channel    = $connection->channel();
 
-$queuesName = 'hello4';
+    // 持久消息不会因为重开机不见true
+    $durable = true;
 
-// 持久消息不会因为重开机不见true
-$durable = true;
-$channel->queue_declare($queuesName, false, $durable, false, false);
+    $channel->queue_declare($queuesName, false, $durable, false, false);
 
-echo " [*] Waiting for messages. To exit press CTRL+C\n";
+    echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
-$callback = function ($msg) {
-    echo ' [x] Received ', $msg->body, "\n";
+    $callback = function (AMQPMessage $req) use ($channel, $queuesName) {
+        $data = $req->body;
+        echo ' [x] Received '.$data."\n";
 
-    // 手动回覆处理完毕
-    $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
-};
+        // 手动回覆处理结果
+        $req->delivery_info['channel']->basic_ack($req->delivery_info['delivery_tag']);
+    };
 
-// 处理并确认前一个消息之前，不要发送新消息
-$channel->basic_qos(null, 1, null);
+    // 处理并确认前一个消息之前，不要发送新消息
+    // $channel->basic_qos(null, 1, null);
 
-// 手动回覆处理结果false，自动回覆true
-$autoAck = false;
-$channel->basic_consume($queuesName, '', false, $autoAck, false, false, $callback);
+    // 手动回覆处理结果false，自动回覆true
+    $autoAck = false;
+    $channel->basic_consume($queuesName, '', false, $autoAck, false, false, $callback);
 
-while (count($channel->callbacks)) {
-    $channel->wait();
+    while (count($channel->callbacks)) {
+        try {
+            $channel->wait(null, false, 5);
+        } catch (AMQPTimeoutException $e) {
+            $channel->close();
+            $connection->close();
+            return true;
+        }
+    }
 }
 
-$channel->close();
-$connection->close();
+$queuesName = 'testQueue';
+
+$run = true;
+while ( $run == true ) {
+    $run = receiveQueue($queuesName);
+}
--- send.php
+++ send.php
@@ -5,18 +5,22 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-$channel = $connection->channel();
+$channel    = $connection->channel();
 
-$queuesName = 'hello4';
+$queuesName = 'testQueue';
 
 // 持久消息不会因为重开机不见true
 $durable = true;
+
 $channel->queue_declare($queuesName, false, $durable, false, false);
 
+// 讯息内容
+$data = json_encode(['status' => 'success', 'data' => ['name' => 'xtony77', 'message' => 'hello world!']]);
+
 // 一般讯息
-// $msg = new AMQPMessage('Hello World 2!');
+// $msg = new AMQPMessage($data);
 // 持久讯息
-$msg = new AMQPMessage('Hello World 2!', array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+$msg = new AMQPMessage($data, array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
 
 // 讯息发送
 $channel->basic_publish($msg, '', $queuesName);
